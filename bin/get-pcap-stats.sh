3f2f079495fd6a353936bef000de066343d7a7b1
vitpodsokhin <vitpodsokhin@gmail.com>
2024-01-02 00:50:19 +0300

    update statistics gathering script

--- bin/get-pcap-stats.sh
+++ bin/get-pcap-stats.sh
@@ -9,34 +9,52 @@ get_dumpfile_endpoints_statistics() {
     local FAMILY=${2}
     local FAMILY=${FAMILY:="ipv4"}
     local DUMP_STATS_PATH="${DUMP_FILE_PATH%.pcapng}.statistics.endpoints.${FAMILY}.txt"
-    # echo "Running: tshark -r $DUMP_FILE_PATH -q -z endpoints,${FAMILY} | tee ${DUMP_STATS_PATH}"
-    # sleep 1
-    # echo "Finished: tshark -r $DUMP_FILE_PATH -q -z endpoints,${FAMILY} | tee ${DUMP_STATS_PATH}"
-    tshark -r "$DUMP_FILE_PATH" -q -z "endpoints,${FAMILY}" | tee "${DUMP_STATS_PATH}"
+
+    # Check if the file exists and is non-empty before overwriting
+    if [ -s "${DUMP_STATS_PATH}" ]; then
+        echo "File ${DUMP_STATS_PATH} already exists and is non-empty. Skipping."
+    else
+# Print info
+cat <<EOF
+Executing:
+tshark -r "${DUMP_FILE_PATH}" -q -z "endpoints,${FAMILY}" | tee "${DUMP_STATS_PATH}"
+EOF
+        # Execution
+        tshark -r "${DUMP_FILE_PATH}" -q -z "endpoints,${FAMILY}" | tee "${DUMP_STATS_PATH}" && \
+# Print info
+cat <<EOF
+Statistics from file "${DUMP_FILE_PATH}" successfully collected to file "${DUMP_STATS_PATH}"
+
+EOF
+    fi
 }
 
 get_dumpfile_phs_statistics() {
     local DUMP_FILE_PATH=$1
     local DUMP_STATS_PATH="${DUMP_FILE_PATH%.pcapng}.statistics.io.phs.txt"
-    # echo "Running: tshark -r $DUMP_FILE_PATH -q -z io,phs | tee ${DUMP_STATS_PATH}"
-    # sleep 1
-    # echo "Finished: tshark -r $DUMP_FILE_PATH -q -z io,phs | tee ${DUMP_STATS_PATH}"
-    tshark -r "$DUMP_FILE_PATH" -q -z io,phs | tee "${DUMP_STATS_PATH}"
-}
 
-# # Process four files at a time
-# batch_size=4
-# count=0
+    # Check if the file exists and is non-empty before overwriting
+    if [ -s "$DUMP_STATS_PATH" ]; then
+        echo "File $DUMP_STATS_PATH already exists and is non-empty. Skipping."
+    else
+# Print info
+cat <<EOF
+Executing:
+tshark -r "${DUMP_FILE_PATH}" -q -z io,phs | tee "${DUMP_STATS_PATH}"
+EOF
+        # Execution
+        tshark -r "${DUMP_FILE_PATH}" -q -z io,phs > "${DUMP_STATS_PATH}" && \
+# Print info
+cat <<EOF
+Statistics from file "${DUMP_FILE_PATH}" successfully collected to file "${DUMP_STATS_PATH}"
+
+EOF
+    fi
+}
 
 for DUMP_FILE_PATH in ${DUMP_FILES_PATHS}; do
     get_dumpfile_phs_statistics "${DUMP_FILE_PATH}"
     for FAMILY in ipv4 ipv6; do
         get_dumpfile_endpoints_statistics "${DUMP_FILE_PATH}" "${FAMILY}"
     done
-
-    # count=$((count + 1))
-    # if [ $count -eq $batch_size ]; then
-    #     wait
-    #     count=0
-    # fi
 done
