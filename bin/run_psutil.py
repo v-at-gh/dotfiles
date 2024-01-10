d6c493d52fb5bd4cbd05a3cd02d5517d8ba6d822
vitpodsokhin <vitpodsokhin@gmail.com>
2024-01-10 15:28:55 +0300

    add system and euid check (psutil needs root privilegies to gather info under macos)

--- bin/run_psutil.py
+++ bin/run_psutil.py
@@ -1,37 +1,51 @@
 #!/usr/bin/env python3
 
-import os, sys
+import sys, os
+
+from os import geteuid
+from sys import exit, platform
 
 try:
     import psutil
 except ModuleNotFoundError:
     print(f'Module psutil not found.\nInstall it with `pip3 install psutil`')
-    sys.exit(1)
+    exit(1)
 
+def check_root_permissions():
+    return platform == 'darwin' and geteuid() != 0
 
-#TODO: fix error encountered under linux:
-# TypeError: '<' not supported between instances of 'NoneType' and 'int'
-connections = psutil.net_connections()
-sorted_pids_of_processes_with_connections = sorted(set(connection.pid for connection in connections if connection.pid != None))
-for i, pid in enumerate(sorted_pids_of_processes_with_connections, 1):
-    try:
-        process_dict = psutil.Process(pid).as_dict()
-        print(f"{i}. {(process_dict['exe'])}")
-        print(f"  Connections amount: {len(process_dict['connections'])}")
-        for connection in process_dict['connections']:
-            if connection.raddr != ():
-                rsocket = f" {connection.raddr.ip}:{connection.raddr.port}"
-            else:
-                rsocket = ''
-            print(
-                f"    {'IPv4' if connection.family == 2 else 'IPv6'}"
-                f" {'TCP' if connection.type == 1 else 'UDP'}"
-                f" {connection.laddr.ip}:{connection.laddr.port} {rsocket}"
-                f" {connection.status}"
-            )
-        print(f"  Open files amount: {len(process_dict['open_files'])}")
-        for open_file in process_dict['open_files']:
-            print(f"    {open_file.fd} {open_file.path}")
-        print()
-    except psutil.NoSuchProcess:
-        pass
+if check_root_permissions():
+    print('Under macOS, you need to run this version as root. Exiting.')
+    exit(2)
+
+def print_processes_with_their_corresponding_connections_and_open_files_as_a_nested_list():
+    connections = psutil.net_connections()
+    sorted_pids_of_processes_with_connections = sorted(set(connection.pid for connection in connections if connection.pid != None))
+    for i, pid in enumerate(sorted_pids_of_processes_with_connections, 1):
+        try:
+            process_dict = psutil.Process(pid).as_dict()
+            print(f"{i}. {(process_dict['exe'])}")
+            print(f"  Connections amount: {len(process_dict['connections'])}")
+            for connection in process_dict['connections']:
+                if connection.raddr != ():
+                    rsocket = f" {connection.raddr.ip}:{connection.raddr.port}"
+                else:
+                    rsocket = ''
+                print(
+                    f"    {'IPv4' if connection.family == 2 else 'IPv6'}"
+                    f" {'TCP' if connection.type == 1 else 'UDP'}"
+                    f" {connection.laddr.ip}:{connection.laddr.port}{rsocket}"
+                    f" {connection.status}"
+                )
+            print(f"  Open files amount: {len(process_dict['open_files'])}")
+            for open_file in process_dict['open_files']:
+                print(f"    {open_file.fd} {open_file.path}")
+            print()
+        except psutil.NoSuchProcess:
+            pass
+
+def main():
+    print_processes_with_their_corresponding_connections_and_open_files_as_a_nested_list()
+
+if __name__ == '__main__':
+    main()
